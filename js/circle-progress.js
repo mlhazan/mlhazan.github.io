beca4f76853f70aa24e23b4b7dcdd9198ede5fb6
mlhazan <mlhazan@hotmail.com>
2016-12-04 22:16:46 -0500

    experiment bar added

--- js/circle-progress.js
+++ js/circle-progress.js
@@ -1,23 +1,8 @@
-$(document).ready(function() {
-    $("#test-circle").circliful({
-        animationStep: 5,
-        foregroundBorderWidth: 5,
-        backgroundBorderWidth: 15,
-        percent: 75
-    });
-});
-$(document).ready(function() {
-
-    $(window).scroll(function() {
-        //if you hard code, then use console
-        //.log to determine when you want the
-        //nav bar to stick.
-        console.log($(window).scrollTop())
-        if ($(window).scrollTop() > 280) {
-            $('#menu').addClass('nav');
-        }
-        if ($(window).scrollTop() < 281) {
-            $('#menu').removeClass('nav');
-        }
-    });
-});
+// $(document).ready(function() {
+//     $("#test-circle").circliful({
+//         animationStep: 5,
+//         foregroundBorderWidth: 5,
+//         backgroundBorderWidth: 15,
+//         percent: 75
+//     });
+// });
